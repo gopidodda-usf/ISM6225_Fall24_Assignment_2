73c65913405ad60d3212bdc6f1ddd25ec0fa7667
gopidodda-usf <gopidodda@usf.edu>
2025-04-06 23:22:54 -0400

    Adds final code for all 8 methods

--- ISM6225_Spring_2024_Assignment_2/Program.cs
+++ ISM6225_Spring_2024_Assignment_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Assignment_2
 {
@@ -62,9 +63,28 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return new List<int>(); // Placeholder
+                if (nums.Length == 0) return new List<int>();   // Edge Case: Empty array
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int index = Math.Abs(nums[i]) - 1;
+                    if (nums[index] > 0)
+                    {
+                        nums[index] = -nums[index];
+                    }
+                }
+
+                List<int> result = new List<int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] > 0)
+                    {
+                        result.Add(i + 1);
+                    }
+                }
+                return result;
             }
+
             catch (Exception)
             {
                 throw;
@@ -76,9 +96,24 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return new int[0]; // Placeholder
+                if (nums.Length == 0) return nums;  // Edge Case: Empty array
+                
+                // Using two-pointer technique for O(1) space complexity
+                int left = 0, right = nums.Length - 1;
+                while (left < right)
+                {
+                    if (nums[left] % 2 > nums[right] % 2)
+                    {
+                        int temp = nums[left];
+                        nums[left] = nums[right];
+                        nums[right] = temp;
+                    }
+                    if (nums[left] % 2 == 0) left++;
+                    if (nums[right] % 2 == 1) right--;
+                }
+                return nums;
             }
+
             catch (Exception)
             {
                 throw;
@@ -90,9 +125,22 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return new int[0]; // Placeholder
+                Dictionary<int, int> map = new Dictionary<int, int>();      // Using a dictionary for O(n) time complexity
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int complement = target - nums[i];
+                    if (map.ContainsKey(complement))
+                    {
+                        return new int[] { map[complement], i };
+                    }
+                    if (!map.ContainsKey(nums[i]))
+                    {
+                        map[nums[i]] = i;
+                    }
+                }
+                return new int[0];
             }
+
             catch (Exception)
             {
                 throw;
@@ -104,9 +152,42 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return 0; // Placeholder
+                if (nums.Length < 3) return 0;  // Edge case: Not enough numbers to form a product
+
+                int max1 = int.MinValue, max2 = int.MinValue, max3 = int.MinValue;
+                int min1 = int.MaxValue, min2 = int.MaxValue;
+
+                foreach (int num in nums)
+                {
+                    if (num > max1)
+                    {
+                        max3 = max2;
+                        max2 = max1;
+                        max1 = num;
+                    }
+                    else if (num > max2)
+                    {
+                        max3 = max2;
+                        max2 = num;
+                    }
+                    else if (num > max3)
+                    {
+                        max3 = num;
+                    }
+
+                    if (num < min1)
+                    {
+                        min2 = min1;
+                        min1 = num;
+                    }
+                    else if (num < min2)
+                    {
+                        min2 = num;
+                    }
+                }
+                return Math.Max(max1 * max2 * max3, max1 * min1 * min2);
             }
+
             catch (Exception)
             {
                 throw;
@@ -118,9 +199,17 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return "101010"; // Placeholder
+                if (decimalNumber == 0) return "0";   // Base case: If decimal number is 0, return "0"
+                    
+                string binary = "";
+                while (decimalNumber > 0)
+                {
+                    binary = (decimalNumber % 2) + binary;
+                    decimalNumber /= 2;
+                }
+                return binary;
             }
+
             catch (Exception)
             {
                 throw;
@@ -132,9 +221,24 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return 0; // Placeholder
+                int left = 0, right = nums.Length - 1;
+
+                // Use binary search to find the minimum element
+                while (left < right)
+                {
+                    int middle = left + (right - left)/2;
+                    if (nums[middle] > nums[right])
+                    {
+                        left = middle + 1;
+                    }
+                    else
+                    {
+                        right = middle;
+                    }
+                }
+                return nums[left];
             }
+
             catch (Exception)
             {
                 throw;
@@ -146,9 +250,18 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return false; // Placeholder
+                if (x < 0) return false;    // Negative numbers are not palindromes
+                
+                int num = x, reverse_num = 0;
+                while (x != 0)
+                {
+                    int y = x % 10;
+                    reverse_num = reverse_num * 10 + y;
+                    x /= 10;
+                }
+                return num == reverse_num;
             }
+
             catch (Exception)
             {
                 throw;
@@ -160,9 +273,19 @@ namespace Assignment_2
         {
             try
             {
-                // Write your code here
-                return 0; // Placeholder
+                if (n == 0) return 0;   // Base case: If n is 0, return 0 as the Fibonacci number
+                if (n == 1) return 1;   // Base case: If n is 1, return 1 as the Fibonacci number
+
+                int a = 0, b = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    int temp = a + b;
+                    a = b;
+                    b = temp;
+                }
+                return b;
             }
+
             catch (Exception)
             {
                 throw;
